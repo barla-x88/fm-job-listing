ccb6a0f99b4511e41cc8c08fa261b3348b60aa56
Barla Ankit <barla.x88@gmail.com>
2024-02-20 21:26:47 +0530

    Implement filter function

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 /* eslint-disable react/prop-types */
 const Filter = (props) => {
   const { data } = props;
@@ -5,16 +7,36 @@ const Filter = (props) => {
   const roles = [...new Set(data.map((job) => job.role))];
   const languages = [...new Set(data.flatMap((job) => job.languages))];
   const filters = [...roles, ...languages];
+  const [currentFilter, setCurrentFilter] = useState([]);
+
+  const filterChange = (e) => {
+    if (e.target.checked) {
+      setCurrentFilter([...currentFilter, e.target.value]);
+    } else {
+      const newFilter = [...currentFilter].filter(
+        (filter) => filter !== e.target.value
+      );
+      setCurrentFilter(newFilter);
+    }
+
+    console.log(currentFilter);
+  };
 
   return (
     <div className="filter">
-      <div className="filter-btns">
+      <form className="filter-boxes">
         {filters.map((filter, index) => (
-          <button key={index} className="filter-btn">
-            {filter}
-          </button>
+          <div key={index}>
+            <label htmlFor={filter}>{filter}</label>
+            <input
+              type="checkbox"
+              value={filter}
+              id={filter}
+              onChange={filterChange}
+            />
+          </div>
         ))}
-      </div>
+      </form>
     </div>
   );
 };
