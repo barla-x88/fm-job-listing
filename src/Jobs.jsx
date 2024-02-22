1a1928b1869d01ab424dfb43755dbdb621a99a3e
Barla Ankit <barla.x88@gmail.com>
2024-02-22 11:35:26 +0530

    Add styles for job cards

--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -25,7 +25,7 @@ const Jobs = (props) => {
 
       {/* Jobs List */}
       {filteredJobs.map((job) => (
-        <div key={job.id} className="job">
+        <div key={job.id} className={job.featured ? 'job featured-job' : 'job'}>
           <div className="job-logo">
             <img src={job.logo} alt={job.company} />
           </div>
@@ -35,17 +35,17 @@ const Jobs = (props) => {
                 <span className="company">{job.company}</span>
               </p>
               <p>
-                {job.new && <span className="is-new">New</span>}
+                {job.new && <span className="is-new">New!</span>}
                 {job.featured && <span className="is-featured">Featured</span>}
               </p>
             </div>
             <div className="job-position">{job.position}</div>
             <div className="job-info">
-              <p>
-                <span className="job-posted">{job.postedAt}</span>
-                <span className="job-contract">{job.contract}</span>
-                <span className="job-location">{job.location}</span>
-              </p>
+              <ul>
+                <li className="job-posted">{job.postedAt}</li>
+                <li className="job-contract">{job.contract}</li>
+                <li className="job-location">{job.location}</li>
+              </ul>
             </div>
           </div>
           <div className="job-tags">
